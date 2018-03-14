eb1475f75b08a05641f1bcd8854cdc3a35bb3eeb
Hafees Falana <hafees.falana@gmail.com>
2018-03-14 13:00:52 +0100

    changed some texts on the home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mini Social Network
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                 <p> This a just a simple social network where users can post articles and comment on other peoples' articles, have profile and edit profile. </p>
                 </div>
             </div>
         </div>
